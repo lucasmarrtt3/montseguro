395e645eb1cc901a977359fdba51e81de7b2e20a
lucasmarrtt3 <lucasschnneider@outlook.com>
2024-07-29 16:02:24 -0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = (
-    ""
+    "postgresql+psycopg2://avnadmin:AVNS_sF19T10fri9zh0ebh4W@montseguro-lucasschnneider009-5267.i.aivencloud.com:15492/defaultdb?sslmode=require"
 )
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
